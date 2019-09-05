5b53551fa0f490656d469ce0a1d2cf805d0af62d
Tianna Vazquez <tiannavazquez@gmail.com>
2019-09-05 06:12:21 -0400

    Added message from Eclipse

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,8 +3,7 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello from Eclipse again.");
 	}
 
 }
